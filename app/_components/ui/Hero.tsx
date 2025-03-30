fcdd5fa4b30129b3c26d257d10aa929aae31fe89
Steven Hutchison <stevenhutchisonkc@gmail.com>
2025-03-29 23:11:03 -0500

    Adjust hero section CTA button layout

--- app/_components/ui/Hero.tsx
+++ app/_components/ui/Hero.tsx
@@ -10,7 +10,7 @@ function Hero() {
             </div>
             <p className="font-heading font-light text-xl md:leading-loose text-transparent bg-gradient-to-tl from-primary to-foreground bg-clip-text py-8 md:text-2xl lg:flex-1 lg:max-w-600 xl:text-3xl xl:leading-[3rem]">
                 Explore a world of culinary creativity at your fingertips. Share your favorite recipes, discover dishes from food lovers around the globe, and connect over the meals that bring us together.</p>
-            <div className="flex w-full justify-center gap-4 md:flex-full md:gap-10">
+            <div className="flex w-full justify-center flex-col sm:flex-row items-center gap-4 md:flex-full md:gap-10">
                 <ActionButtons />
             </div>
         </section>
