623119c7be16e83802a236ca7ca72397b55adfea
Steven Hutchison <stevenhutchisonkc@gmail.com>
2025-04-22 10:49:50 -0500

    Fix login redirect links to production mode

--- app/_utils/constants.tsx
+++ app/_utils/constants.tsx
@@ -1,9 +1,9 @@
 // Production
 
-// export const loginLink = 'https://auth.platelette.com/login?client_id=27a4hidci87i37h6ufhdg5mo4&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https%3A%2F%2Fwww.platelette.com%2Fauth-callback';
-// export const logoutLink = 'https://auth.platelette.com/logout?client_id=27a4hidci87i37h6ufhdg5mo4&scope=aws.cognito.signin.user.admin+email+openid+profile&logout_uri=https%3A%2F%2Fwww.platelette.com%2Flogout';
+export const loginLink = 'https://auth.platelette.com/login?client_id=27a4hidci87i37h6ufhdg5mo4&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https%3A%2F%2Fwww.platelette.com%2Fauth-callback';
+export const logoutLink = 'https://auth.platelette.com/logout?client_id=27a4hidci87i37h6ufhdg5mo4&scope=aws.cognito.signin.user.admin+email+openid+profile&logout_uri=https%3A%2F%2Fwww.platelette.com%2Flogout';
 
 // Development
 
-export const loginLink = 'https://auth.platelette.com/login?client_id=27a4hidci87i37h6ufhdg5mo4&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https%3A%2F%2Flocalhost:3000%2Fauth-callback';
-export const logoutLink = 'https://auth.platelette.com/logout?client_id=27a4hidci87i37h6ufhdg5mo4&scope=aws.cognito.signin.user.admin+email+openid+profile&logout_uri=https%3A%2F%2Flocalhost:3000%2Flogout';
+// export const loginLink = 'https://auth.platelette.com/login?client_id=27a4hidci87i37h6ufhdg5mo4&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https%3A%2F%2Flocalhost:3000%2Fauth-callback';
+// export const logoutLink = 'https://auth.platelette.com/logout?client_id=27a4hidci87i37h6ufhdg5mo4&scope=aws.cognito.signin.user.admin+email+openid+profile&logout_uri=https%3A%2F%2Flocalhost:3000%2Flogout';
