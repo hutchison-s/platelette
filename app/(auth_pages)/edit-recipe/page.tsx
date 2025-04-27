957209a68e638784b8b9025330324bcebe299544
Steven Hutchison <stevenhutchisonkc@gmail.com>
2025-04-27 12:13:23 -0500

    Add suspense boundary for edit form

--- app/(auth_pages)/edit-recipe/page.tsx
+++ app/(auth_pages)/edit-recipe/page.tsx
@@ -1,7 +1,8 @@
 import PageWrapper from '@/app/_components/ui/PageWrapper'
-import React from 'react'
+import React, { Suspense } from 'react'
 import RecipeEditForm from '../account/RecipeEditForm'
 import Link from 'next/link'
+import { Loader } from 'lucide-react'
 
 function EditPage() {
   return (
@@ -9,7 +10,14 @@ function EditPage() {
         <Link href={'/account'} className="block text-md text-primary hover:underline py-4">
             &larr; Back to Account Page
         </Link>
-        <RecipeEditForm />
+        <Suspense fallback={
+            <div className='w-full min-h-[400px] flex flex-col justify-center items-center gap-4'>
+                <h2 className='font-heading text-3xl font-bold text-foreground'>Loading Recipe Edit Form...</h2>
+                <Loader size={80} className='animate-spin text-primary my-6 mx-auto'/>
+            </div>
+        }>
+            <RecipeEditForm />
+        </Suspense>
         </PageWrapper>
   )
 }
